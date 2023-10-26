bd0f7c005cedb5eec2e569dd61665952b76ecb5d
iliafatemi <fatemiilia4@gmail.com>
2023-10-25 18:19:54 -0700

    done functions

--- sTalkFunctions.c
+++ sTalkFunctions.c
@@ -26,14 +26,22 @@ char *HOSTNAME;
 int socketDescriptor;
 bool CHAT_ACTIVE = true;
 bool WAITING_TO_RECEIVE = true;
-bool WAITING_TO_SEND = true;
+bool TYPING_MSG = true;
 
 
 void *keyboard(){
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
     char message[MAXBUFF];
     while(CHAT_ACTIVE){
-            pthread_mutex_lock(&syncLocalMutex);
-        while(WAITING_TO_SEND){
+        pthread_testcancel();
+        pthread_mutex_lock(&syncLocalMutex);
+        //wait to send message
+        while(!TYPING_MSG){
+            pthread_testcancel();
+            pthread_cond_wait(&syncLocalCond,&syncLocalMutex);
+        } 
+        while(TYPING_MSG){
+            pthread_testcancel();
             fgets(message, MAXBUFF, stdin);
             printf("\033[32mYou: \033[0m%s\n", message);
             if(message[0] == '!'){
@@ -42,8 +50,9 @@ void *keyboard(){
             }
             List_append(localMsgList, message);
             memset(message,'\0',MAXBUFF);
-            WAITING_TO_SEND = false;
+            TYPING_MSG = false;
             pthread_mutex_unlock(&syncLocalMutex);
+            pthread_cond_signal(&syncLocalCond);
         }
         
     }
@@ -52,15 +61,24 @@ void *keyboard(){
 
 
 void *screen(){
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
     while(CHAT_ACTIVE){
+        pthread_testcancel();
         pthread_mutex_lock(&syncRemoteMutex);
+        while(WAITING_TO_RECEIVE){
+            pthread_testcancel();
+            pthread_cond_wait(&syncRemoteCond,&syncRemoteMutex);
+        }
         while(!WAITING_TO_RECEIVE){
+            pthread_testcancel();
             while(List_count(remoteMsgList) > 0){
+                pthread_testcancel();
                 List_first(remoteMsgList);
                 printf("\033[34m%s: \033%s\n", HOSTNAME, (char*)List_remove(remoteMsgList));
             }
         }
         pthread_mutex_unlock(&syncRemoteMutex);
+        pthread_cond_signal(&syncRemoteCond);
         WAITING_TO_RECEIVE = true;
     }
     pthread_exit(NULL);
@@ -68,17 +86,26 @@ void *screen(){
 
 
 void *send_msg(){
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
     char msg_content[MAXBUFF];
     while(CHAT_ACTIVE){
+        pthread_testcancel();
         pthread_mutex_lock(&syncLocalMutex);
-        while(!WAITING_TO_SEND){
+        while(TYPING_MSG){
+            pthread_testcancel();
+            pthread_cond_wait(&syncLocalCond,&syncLocalMutex);
+        }
+        while(!TYPING_MSG){
+            pthread_testcancel();
             while(List_count(localMsgList) > 0 && List_first(localMsgList) != NULL && List_curr(localMsgList) != NULL){
+                pthread_testcancel();
                 List_first(localMsgList);
                 strcpy(msg_content, (char*)List_remove(localMsgList));
                 sendto(socketDescriptor, msg_content, MAXBUFF, 0, (struct sockaddr*)&remoteSin, sizeof(struct sockaddr_in));
             }
             pthread_mutex_unlock(&syncLocalMutex);
-            WAITING_TO_SEND = true;
+            pthread_cond_signal(&syncLocalCond);
+            TYPING_MSG = true;
         }
     }
     pthread_exit(NULL);
@@ -86,15 +113,23 @@ void *send_msg(){
 
 
 void *receive(){
-    char message[MAXBUFF];
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
+    char received_message[MAXBUFF];
     socklen_t remote_length = sizeof(remoteSin);
     while(CHAT_ACTIVE){
+        pthread_testcancel();
         pthread_mutex_lock(&syncRemoteMutex);
+        while(!WAITING_TO_RECEIVE){
+            pthread_testcancel();
+            pthread_cond_wait(&syncRemoteCond,&syncRemoteMutex);
+        }
         while(WAITING_TO_RECEIVE){
-            recvfrom(socketDescriptor, message, MAXBUFF, 0, (struct sockaddr*)&remoteSin, &remote_length);
-            List_append(remoteMsgList, message);
+            pthread_testcancel();
+            recvfrom(socketDescriptor, received_message, MAXBUFF, 0, (struct sockaddr*)&remoteSin, &remote_length);
+            List_append(remoteMsgList, received_message);
             WAITING_TO_RECEIVE = false;
             pthread_mutex_unlock(&syncRemoteMutex);
+            pthread_cond_signal(&syncRemoteCond);
         }
     }
     pthread_exit(NULL);
